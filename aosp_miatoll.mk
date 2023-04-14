f938ce4cf349a7baa9ec09d48fdea2e91c6772b0
sajidshahriar <sazidshahriar39@gmail.com>
2023-04-14 14:54:07 +0000

    miatoll: build NGA, Google recorder and live wallpapers
    Change-Id: I6bcba4ad32132b8db9c9665ca982fc626edec4ad

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -14,6 +14,10 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
+# Flags
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
