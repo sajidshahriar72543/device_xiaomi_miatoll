90975ad6c6a62c98db15d14df6fb7cd4d03620ce
sajidshahriar <sazidshahriar39@gmail.com>
2023-04-27 11:50:03 +0000

    miatoll: overlay: SM6250 >> Miatoll
    Change-Id: I195eb9bbf1f6db0e5841bcf4f3e9b852dc77d565

--- device.mk
+++ device.mk
@@ -263,9 +263,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     ApertureOverlay \
     CarrierConfigResCommon \
-    SM6250Frameworks \
-    SM6250SystemUI \
-    SM6250Settings \
+    MiatollFrameworks \
+    MiatollSystemUI \
+    MiatollSettings \
     TelephonyResCommon \
     WifiOverlay
 
