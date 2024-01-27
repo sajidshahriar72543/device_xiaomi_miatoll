6d70431905179cd3bcb6c9c548ab253b2086e9ee
Sajid Shahriar <63386478+sajidshahriar72543@users.noreply.github.com>
2024-01-28 00:14:43 +0600

    miatoll: switch to QuickSilver Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,7 +114,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
 
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := cust_defconfig
+TARGET_KERNEL_CONFIG := vendor/miatoll-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/miatoll
 
 KERNEL_LD := LD=ld.lld
