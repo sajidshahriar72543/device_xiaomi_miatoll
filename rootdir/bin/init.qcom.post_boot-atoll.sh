66c64ccb0f19aee00d7ef600f25e3f367e92237c
clarencelol <clarencekuiek@proton.me>
2024-08-01 23:43:39 +0800

    miatoll: rootdir: Nuked hispeed_*
    * These are for CAF's WALT configuration anyway, we are using PELT
    
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- rootdir/bin/init.qcom.post_boot-atoll.sh
+++ rootdir/bin/init.qcom.post_boot-atoll.sh
@@ -67,20 +67,13 @@ echo 1 > /proc/sys/kernel/sched_energy_aware
 echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 500 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
 echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
-echo 1248000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
 echo 576000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
 # configure governor settings for big cluster
 echo "schedutil" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
 echo 500 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/up_rate_limit_us
 echo 20000 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/down_rate_limit_us
-echo 1267200 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_freq
 echo 652800 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq
-
-# sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
-echo -6 >  /sys/devices/system/cpu/cpu6/sched_load_boost
-echo -6 >  /sys/devices/system/cpu/cpu7/sched_load_boost
-echo 85 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_load
 
 # Enable bus-dcvs
 for device in /sys/devices/platform/soc
