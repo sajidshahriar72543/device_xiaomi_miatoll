c108d1ddd034677d0eba77a321dd9654493a5634
clarencelol <clarencekuiek@proton.me>
2024-10-19 11:57:04 +0800

    miatoll: Drop Misys HAL
    * It is in miuicamera repo now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,8 +181,6 @@ VENDOR_SECURITY_PATCH := 2023-05-01
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 -include vendor/xiaomi/miuicamera/SEPolicy.mk
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Shims
